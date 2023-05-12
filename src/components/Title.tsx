1dff9a3ec9b8a43a935359224fe22bf1bf72b200
DaniilBatiuk <sportak50@gmail.com>
2023-05-12 16:23:43 +0300

    Undate Title style

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { TitleText } from "../types/types";
-
+import "../styles/Title.scss";
 
 const Title: React.FC<TitleText> = ({ titleText,classNames }: TitleText) =>  {
   return (
