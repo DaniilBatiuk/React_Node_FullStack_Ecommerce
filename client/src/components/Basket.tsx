f03e4fc315d5419c8c1606c5c30772f6e63be85e
DaniilBatiuk <sportak50@gmail.com>
2023-06-08 12:54:20 +0300

    Update Basket scss

--- client/src/components/Basket.tsx
+++ client/src/components/Basket.tsx
@@ -35,148 +35,15 @@ const Basket: React.FC<BasketProps> = ({ active, setActive }: BasketProps) => {
                             </div>
                         </div>
                     </div>
-                    <div className="basket__item">
-                        <div className="basket__photo">
-                            <img src={photo} />
+                    <div className="basket__footer">
+                        <div className="basket__subtotal subtotal">
+                            <div className="subtotal__text">Subtotal</div>
+                            <div className="subtotal__price">45.00$</div>
                         </div>
-                        <div className="basket__info">
-                            <div className="basket__info-close">
-                                <div className="basket__item-name">Text text etxt text text tettxttextxt</div>
-                                <svg width="24" height="24" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-                                    <path fillRule="evenodd" clipRule="evenodd" d="M4.11 2.697L2.698 4.11 6.586 8l-3.89 3.89 1.415 1.413L8 9.414l3.89 3.89 1.413-1.415L9.414 8l3.89-3.89-1.415-1.413L8 6.586l-3.89-3.89z" fill="#000"></path>
-                                </svg>
-                            </div>
-                            <div className="basket__price-quantity">
-                                <div className="basket__quantity">
-                                    <div className="basket__quantity-block">
-                                        <button className="basket__quantity-button" onClick={() => { if (count > 1) { setCount(prev => prev - 1); } }}>-</button>
-                                        <div className="basket__quantity-text">{count}</div>
-                                        <button className="basket__quantity-button" onClick={() => setCount(prev => prev + 1)}>+</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="basket__item">
-                        <div className="basket__photo">
-                            <img src={photo} />
-                        </div>
-                        <div className="basket__info">
-                            <div className="basket__info-close">
-                                <div className="basket__item-name">Text text etxt text text tettxttextxt</div>
-                                <svg width="24" height="24" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-                                    <path fillRule="evenodd" clipRule="evenodd" d="M4.11 2.697L2.698 4.11 6.586 8l-3.89 3.89 1.415 1.413L8 9.414l3.89 3.89 1.413-1.415L9.414 8l3.89-3.89-1.415-1.413L8 6.586l-3.89-3.89z" fill="#000"></path>
-                                </svg>
-                            </div>
-                            <div className="basket__price-quantity">
-                                <div className="basket__quantity">
-                                    <div className="basket__quantity-block">
-                                        <button className="basket__quantity-button" onClick={() => { if (count > 1) { setCount(prev => prev - 1); } }}>-</button>
-                                        <div className="basket__quantity-text">{count}</div>
-                                        <button className="basket__quantity-button" onClick={() => setCount(prev => prev + 1)}>+</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="basket__item">
-                        <div className="basket__photo">
-                            <img src={photo} />
-                        </div>
-                        <div className="basket__info">
-                            <div className="basket__info-close">
-                                <div className="basket__item-name">Text text etxt text text tettxttextxt</div>
-                                <svg width="24" height="24" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-                                    <path fillRule="evenodd" clipRule="evenodd" d="M4.11 2.697L2.698 4.11 6.586 8l-3.89 3.89 1.415 1.413L8 9.414l3.89 3.89 1.413-1.415L9.414 8l3.89-3.89-1.415-1.413L8 6.586l-3.89-3.89z" fill="#000"></path>
-                                </svg>
-                            </div>
-                            <div className="basket__price-quantity">
-                                <div className="basket__quantity">
-                                    <div className="basket__quantity-block">
-                                        <button className="basket__quantity-button" onClick={() => { if (count > 1) { setCount(prev => prev - 1); } }}>-</button>
-                                        <div className="basket__quantity-text">{count}</div>
-                                        <button className="basket__quantity-button" onClick={() => setCount(prev => prev + 1)}>+</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="basket__item">
-                        <div className="basket__photo">
-                            <img src={photo} />
-                        </div>
-                        <div className="basket__info">
-                            <div className="basket__info-close">
-                                <div className="basket__item-name">Text text etxt text text tettxttextxt</div>
-                                <svg width="24" height="24" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-                                    <path fillRule="evenodd" clipRule="evenodd" d="M4.11 2.697L2.698 4.11 6.586 8l-3.89 3.89 1.415 1.413L8 9.414l3.89 3.89 1.413-1.415L9.414 8l3.89-3.89-1.415-1.413L8 6.586l-3.89-3.89z" fill="#000"></path>
-                                </svg>
-                            </div>
-                            <div className="basket__price-quantity">
-                                <div className="basket__quantity">
-                                    <div className="basket__quantity-block">
-                                        <button className="basket__quantity-button" onClick={() => { if (count > 1) { setCount(prev => prev - 1); } }}>-</button>
-                                        <div className="basket__quantity-text">{count}</div>
-                                        <button className="basket__quantity-button" onClick={() => setCount(prev => prev + 1)}>+</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="basket__item">
-                        <div className="basket__photo">
-                            <img src={photo} />
-                        </div>
-                        <div className="basket__info">
-                            <div className="basket__info-close">
-                                <div className="basket__item-name">Text text etxt text text tettxttextxt</div>
-                                <svg width="24" height="24" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-                                    <path fillRule="evenodd" clipRule="evenodd" d="M4.11 2.697L2.698 4.11 6.586 8l-3.89 3.89 1.415 1.413L8 9.414l3.89 3.89 1.413-1.415L9.414 8l3.89-3.89-1.415-1.413L8 6.586l-3.89-3.89z" fill="#000"></path>
-                                </svg>
-                            </div>
-                            <div className="basket__price-quantity">
-                                <div className="basket__quantity">
-                                    <div className="basket__quantity-block">
-                                        <button className="basket__quantity-button" onClick={() => { if (count > 1) { setCount(prev => prev - 1); } }}>-</button>
-                                        <div className="basket__quantity-text">{count}</div>
-                                        <button className="basket__quantity-button" onClick={() => setCount(prev => prev + 1)}>+</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="basket__item">
-                        <div className="basket__photo">
-                            <img src={photo} />
-                        </div>
-                        <div className="basket__info">
-                            <div className="basket__info-close">
-                                <div className="basket__item-name">Text text etxt text text tettxttextxt</div>
-                                <svg width="24" height="24" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-                                    <path fillRule="evenodd" clipRule="evenodd" d="M4.11 2.697L2.698 4.11 6.586 8l-3.89 3.89 1.415 1.413L8 9.414l3.89 3.89 1.413-1.415L9.414 8l3.89-3.89-1.415-1.413L8 6.586l-3.89-3.89z" fill="#000"></path>
-                                </svg>
-                            </div>
-                            <div className="basket__price-quantity">
-                                <div className="basket__quantity">
-                                    <div className="basket__quantity-block">
-                                        <button className="basket__quantity-button" onClick={() => { if (count > 1) { setCount(prev => prev - 1); } }}>-</button>
-                                        <div className="basket__quantity-text">{count}</div>
-                                        <button className="basket__quantity-button" onClick={() => setCount(prev => prev + 1)}>+</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="basket__footer">
-                    <div className="basket__subtotal subtotal">
-                        <div className="subtotal__text">Subtotal</div>
-                        <div className="subtotal__price">45.00$</div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
+            );
 }
 
-export default Basket;
+            export default Basket;
